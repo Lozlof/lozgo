125aeebe7c6d7331d766e2ef0b82dfe6b58a4a22
Justin <lozlofcyber@outlook.com>
2024-10-26 13:30:22 -0400

    Update and rename loz.go to sanitization.go

--- sanitization.go
+++ sanitization.go
@@ -6,7 +6,9 @@ import (
 )
 
 // In Go, "error" is a predefined type.
-func SanitizationIntInRange(number, minNum, maxNum int) (int, error) { 
+// Takes a string input and two int's.
+// The string is sent off to sanitizationIntVerify to be verified and converted to an int.
+func SanitizationIntInRange(input string, minNum, maxNum int) (int, error) { 
 
 	log.SetPrefix("Function: SanitizationIntInRange: ")
 	log.SetFlags(2)
@@ -14,6 +16,10 @@ func SanitizationIntInRange(number, minNum, maxNum int) (int, error) {
 	// Declares the variables verifiedInt and err.
 	// The variables are initialized when sanitizationIntVerify returns the values.
 	verifiedInt, err := sanitizationIntVerify(number)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func sanitizationIntVerify(number int) (int, error) {
