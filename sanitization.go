7c7c78286782a33e0cee8e92696d1615321eaf95
Lozlof <lozlofcyber@outlook.com>
2024-10-26 18:10:27 +0000

    Update on the progress

--- sanitization.go
+++ sanitization.go
@@ -3,6 +3,7 @@ package lozgo
 import (
 	"errors"
 	"log"
+	"strconv"
 )
 
 // In Go, "error" is a predefined type.
@@ -10,8 +11,21 @@ import (
 // The string is sent off to sanitizationIntVerify to be verified and converted to an int.
 func SanitizationIntInRange(input string, minNum, maxNum int) (int, error) { 
 
-	log.SetPrefix("Function: SanitizationIntInRange: ")
+	// Set logging values.
+	log.SetPrefix("lozgo/SanitizationIntInRange: ")
 	log.SetFlags(2)
+
+	// Attempts to convert "input" to an integer.
+	// If it is converted, err will be nil.
+	// convertedInt will be initialized to the integer returned by strconv.Atoi.
+	convertedInt, err := strconv.Atoi(input)
+
+	if err != nil {
+		log.Println('"',input,'"'," is not an integer" )
+		return 0, errors.New("input cannot be converted to an integer")
+	}
+
+
 
 	// Declares the variables verifiedInt and err.
 	// The variables are initialized when sanitizationIntVerify returns the values.
